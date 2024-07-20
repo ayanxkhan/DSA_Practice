e0b68b44290b2c096fc7c81e744dc1d3f645b8de
Ayan Khan <ayanxkhan12@gmail.com>
2024-07-20 20:28:49 +0530

    changes

--- basics/functions.cpp
+++ basics/functions.cpp
@@ -100,3 +100,21 @@ int main(){
   
 
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// string vote(int x){
+//     if(x<18){
+//         return "Cannot vote" ;
+//     }
+//     else{
+//         return "Can vote";
+//     }
+// }
+
+// int main(){
+//     int age;
+//     cin >> age;
+//     cout << vote(age);
+//     return 0;
+// }
--- demo1.cpp
+++ demo1.cpp
@@ -1,11 +1,18 @@
-#include<bits/stdc++.h> 
+#include<bits/stdc++.h>
 using namespace std;
 
+string vote(int x){
+    if(x<18){
+        return "Cannot vote" ;
+    }
+    else{
+        return "Can vote";
+    }
+}
+
 int main(){
-    string str="Ayan";
-    int len=str.size();
-    // cout << len;
-    // cout << str[0];
-    cout << str[len-1];
-    return 0;    
+    int age;
+    cin >> age;
+    cout << vote(age);
+    return 0;
 }
